3c5176af48a796a9a546e4e6c72129962c13a15d
tinzupancic <tin.zupancic@turingstudio.eu>
2023-02-23 19:09:24 +0100

    Initial project commit

---
+++ Source/LicenseSpring/LicenseSpringGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LicenseSpringGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LICENSESPRING_API ALicenseSpringGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

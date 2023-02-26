b93d991fdd9c284289852a6ab609b39a39cacb6f
tinzupancic <tin.zupancic@turingstudio.eu>
2023-02-26 14:17:18 +0100

    Added windows libraries

--- Plugins/LicenseSpringPlugin/Source/ThirdParty/LicenseSpringPluginLibrary/LicenseSpringPluginLibrary.Build.cs
+++ Plugins/LicenseSpringPlugin/Source/ThirdParty/LicenseSpringPluginLibrary/LicenseSpringPluginLibrary.Build.cs
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 using UnrealBuildTool;
 
 public class LicenseSpringPluginLibrary : ModuleRules
@@ -13,56 +15,52 @@ public class LicenseSpringPluginLibrary : ModuleRules
 			new string[] {
                 "LicenseSpringPlugin/Public",
                 "LicenseSpringPlugin/ThirdParty/LicenseSpringPluginLibrary"
-				// ... add public include paths required here ...
 			}
-			);
+		);
 
 		PrivateIncludePaths.AddRange(
 			new string[] {
                 "LicenseSpring/Private"
-				// ... add other private include paths required here ...
 			}
-			);
+		);
 
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			// Add the import library
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "ExampleLibrary.lib"));
+			string thirdPartyDir = Path.Combine(ModuleDirectory, "x64", "Release");
 
-			// Delay-load the DLL, so we can load it from the right place first
-			PublicDelayLoadDLLs.Add("ExampleLibrary.dll");
+			// Add the import library
+			PublicAdditionalLibraries.Add(Path.Combine(thirdPartyDir, "LicenseSpring.lib"));
+
+			var libs = new List<string>() 
+			{
+				"LicenseSpring.dll",
+				"libcrypto-1_1-x64.dll",
+				"libcurl.dll",
+				"libssl-1_1-x64.dll",
+				"LSVMD.dll"
+			};
+
+			libs.ForEach(lib => PublicDelayLoadDLLs.Add(Path.Combine(thirdPartyDir, lib)));
 
 			// Ensure that the DLL is staged along with the executable
 			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/LicenseSpringPluginLibrary/Win64/ExampleLibrary.dll");
         }
         else if (Target.Platform == UnrealTargetPlatform.Mac)
         {
+			var libs = new List<string>() 
+			{ 
+				"libcrypto.1.1.dylib", 
+				"libcrypto.dylib", 
+				"libcurl.4.dylib", 
+				"libcurl.dylib", 
+				"libLicenseSpring.dylib", 
+				"libssl.1.1.dylib", 
+				"libssl.dylib"
+			};
+
 			string thirdPartyDir = Path.Combine(ModuleDirectory, "Mac", "Release");
-            PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libcrypto.1.1.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libcrypto.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libcurl.4.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libcurl.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libLicenseSpring.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libssl.1.1.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libssl.dylib"));
-            //RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libcrypto.1.1.dylib");
-			//RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libcrypto.dylib");
-			//RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libcurl.4.dylib");
-			//RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libcurl.dylib");
-			// RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libLicenseSpring.dylib");
-			//RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libssl.1.1.dylib");
-			//RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LicenseSpringPluginLibrary/Mac/Release/libssl.dylib");
-
-			// string binariesDir = Path.Combine(ModuleDirectory, "../../Binaries/Mac/");
-        	// Directory.CreateDirectory(binariesDir);
-			// File.Copy(Path.Combine(thirdPartyDir, "libcrypto.1.1.dll"), Path.Combine(binariesDir, "libcrypto.1.1.dll"), true);
-			// File.Copy(Path.Combine(thirdPartyDir, "libcrypto.dll"), Path.Combine(binariesDir, "libcrypto.dll"), true);
-			// File.Copy(Path.Combine(thirdPartyDir, "libcurl.4.dll"), Path.Combine(binariesDir, "libcurl.4.dll"), true);
-			// File.Copy(Path.Combine(thirdPartyDir, "libcurl.dll"), Path.Combine(binariesDir, "libcurl.dll"), true);
-			// File.Copy(Path.Combine(thirdPartyDir, "libLicenseSpring.dll"), Path.Combine(binariesDir, "libLicenseSpring.dll"), true);
-			// File.Copy(Path.Combine(thirdPartyDir, "libssl.1.1.dll"), Path.Combine(binariesDir, "libssl.1.1.dll"), true);
-			// File.Copy(Path.Combine(thirdPartyDir, "libssl.dll"), Path.Combine(binariesDir, "libssl.dll"), true);
-
+			libs.ForEach(lib => PublicDelayLoadDLLs.Add(Path.Combine(thirdPartyDir, lib)));
+			libs.ForEach(lib => RuntimeDependencies.Add(Path.Combine(thirdPartyDir, lib)));
         }
         else if (Target.Platform == UnrealTargetPlatform.Linux)
 		{

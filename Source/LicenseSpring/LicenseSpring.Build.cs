be2bc8a75970827456483e6f6123e4a77fe26180
Filip Tušek <filip.tusek@turingstudio.eu>
2023-03-01 11:58:50 +0100

    Fixed include problem

--- Source/LicenseSpring/LicenseSpring.Build.cs
+++ Source/LicenseSpring/LicenseSpring.Build.cs
@@ -10,7 +10,9 @@ public class LicenseSpring : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "LicenseSpringPlugin" });
+		
+		//PrivateIncludePaths.AddRange(new string[]{""});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
